fe615745c78be4b5de3450714b4a9f456c3b4a73
Philip Bao <bwwyyy@users.noreply.github.com>
2018-04-14 18:45:04 -0400

    Padding dummy particle (#4)
    * add support to padding particle if number is bad
    
    * add coner case check
    
    * fix comments and Header file name
    
    * make particle img larger
    
    * change result print format
    
    * comment clean
    
    * clean up comment 2
    
    * validation testing
    
    * updating testing for percent difference.

--- src/define.h
+++ src/define.h
@@ -21,17 +21,18 @@
 
 /*--------------------- Data structure ----------------------*/
 // Particle
-#define PARTICLE_PROPERTIES_COUNT 6 // p_id, p_weight, p_pos_x, p_pos_y, p_vol_x, p_vol_y
+#define PARTICLE_PROPERTIES_COUNT 7 // p_id, p_weight, p_pos_x, p_pos_y, p_vol_x, p_vol_y, p_type
 #define ID_COL 0
 #define WEIGHT_COL 1
 #define POS_X_COL 2
 #define POS_Y_COL 3
 #define VOL_X_COL 4
 #define VOL_Y_COL 5
+#define TYPE_COL 6
 
 // Force
 #define FORCE_PROPERTIES_COUNT 4 // force_x, force_y, force_to(p_id), force_from(p_id)
-#define FORCE_SUM_PROPERTIES_COUNT 2 // force_x, force_y, force_to(p_id)
+#define FORCE_SUM_PROPERTIES_COUNT 2 // force_x, force_y
 #define X_COL 0
 #define Y_COL 1
 #define TO_COL 2
@@ -43,6 +44,8 @@
 #define LIGHT 0
 #define MEDIUM 1
 #define HEAVY 2
+#define DUMMY 3
+#define DUMMY_WEIGHT 0.0
 #define MASTER_TO_SLAVE_TAG 883216 // arbitrary tag for messages sent from master to slaves
 #define SLAVE_TO_MASTER_TAG 886123 // arbitrary tag for messages sent from slaves to master
 #define SLAVE_TO_SLAVE_TAG 666123 // arbitrary tag for messages sent from slaves to master
